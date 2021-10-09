1c63aec6f352b357b3f7d56fe0c156b8f7208b9b
Itamar Takeo <itamar.abe@gmail.com>
2021-10-09 10:52:00 -0300

    Menu de métodos e classe de serviços

--- BankApp/application/Program.cs
+++ BankApp/application/Program.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 
 using BankApp.entities;
 using System.Linq;
+using BankApp.services;
 
 namespace BankApp
 {
@@ -14,63 +15,90 @@ namespace BankApp
         static void Main(string[] args)
         {
 
-            var fileName = "accounts.csv"; //Nome do arquivo
-            var path = Path.Combine(
-                            Environment.GetEnvironmentVariable("USERPROFILE"), // Pasta local do usuário
-                            @"source\repos\BankApp\BankApp\", // Local do projeto a partir da pasta do usuário
-                            fileName);
-            try
-            {
-                var reader = new StreamReader(path);
-                while (!reader.EndOfStream)
-                {
-                    var line = reader.ReadLine();
-                    var values = line.Split(';');
+            BankServices.Services.Add("Terminar operação", EndApplication);
+            BankServices.Services.Add("Importar aquivo existente", ImportFile);
+            BankServices.Services.Add("Adicionar novo cliente", AddNewClient);
+            BankServices.Services.Add("Salvar o arquivo com novos dados", SaveFile);
+            BankServices.Services.Add("Ver cliente mais antigo", SeeOldestClient);
+            BankServices.Services.Add("Ver clientes descapitalizados", SeePoorClients);
 
-                    Bank.Register(new Person(
-                        values[0], // Name
-                        int.Parse(values[1]), // AccNumber
-                        DateTime.Parse(values[2]), // AccDate
-                        double.Parse(values[3], new CultureInfo("en-US")) // Balance
-                                    ));
-                }
-                Console.WriteLine("Import successful!");
-            }
-            catch (Exception e) {
-                Console.WriteLine(e.Message);
+            int i = 0;
+            foreach (var item in BankServices.Services)
+            {
+                Console.WriteLine("{0} - {1}", i, item.Key);
+                i++;
             }
 
-        // Bloco de adicionar novo cliente com entrada de dados
-        // Nome do cliente
-        Console.Write("Name: ");
-            string name = Console.ReadLine();
-
-        // Número da conta do cliente sem permitir repetição
-        Console.Write("Account Number: ");
-            int accNumber = int.Parse(Console.ReadLine());
-            if (Bank.ExistingAccount(accNumber))
+            int a;
+            do
             {
-                Console.WriteLine("This account number is already in use. Try again: ");
+                Console.Write("Enter service number: ");
+                a = int.Parse(Console.ReadLine());
+                Console.WriteLine("Você selecionou: {0}", BankServices.Services.ElementAt(a).Key);
+                BankServices.RunService(a);
+            } while (a != 0);
+
+            // Bloco de leitura de dados de arquivo em pasta local
+            static void ImportFile()
+            {
+                var fileName = "accounts.csv"; //Nome do arquivo
+                var path = Path.Combine(
+                                Environment.GetEnvironmentVariable("USERPROFILE"), // Pasta local do usuário
+                                @"source\repos\BankApp\BankApp\", // Local do projeto a partir da pasta do usuário
+                                fileName);
+                Console.WriteLine(BankServices.ImportData(path));
+            }
+
+            // Encerrar aplicação
+            static void SaveFile()
+            {
+                BankServices.SaveData();
+            }
+
+            // Bloco de adicionar novo cliente com entrada de dados
+            static void AddNewClient()
+            {
+                // Nome do cliente
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+
+                // Número da conta do cliente sem permitir repetição
                 Console.Write("Account Number: ");
-                accNumber = int.Parse(Console.ReadLine());
-    }
+                int accNumber = int.Parse(Console.ReadLine());
+                if (Bank.ExistingAccount(accNumber))
+                {
+                    Console.WriteLine("This account number is already in use. Try again: ");
+                    Console.Write("Account Number: ");
+                    accNumber = int.Parse(Console.ReadLine());
+                }
 
-    // Data de criação no momento de execução
-    DateTime accDate = DateTime.Now;
+                // Data de criação no momento de execução
+                DateTime accDate = DateTime.Now;
 
-    // Saldo inicial da conta
-    Console.Write("Initial balance: ");
-            double balance = double.Parse(Console.ReadLine());
+                // Saldo inicial da conta
+                Console.Write("Initial balance: ");
+                double balance = double.Parse(Console.ReadLine());
 
-    // Instanciação de um novo cliente na lista de clientes do banco
-    Bank.Register(new Person(name, accNumber, accDate, balance));
+                // Instanciação de um novo cliente na lista de clientes do banco
+                Bank.Register(new Person(name, accNumber, accDate, balance));
+            }
 
             // Ver cliente mais antigo registrado
-            Console.WriteLine($"First client: {Bank.FirstClient()}");
+            static void SeeOldestClient()
+            {
+                Console.WriteLine($"First client: {Bank.FirstClient()}");
+            }
 
             // Ver clientes com saldo menor que 100
-            Bank.PoorClients().ForEach(p => Console.WriteLine(p.ToString()));
+            static void SeePoorClients()
+            {
+                Bank.PoorClients().ForEach(p => Console.WriteLine(p.ToString()));
+            }
 
+            // Encerrar aplicação
+            static void EndApplication()
+            {
+            }
         }
     }
 }
---
+++ BankApp/services/BankServices.cs
@@ -0,0 +1,53 @@
+﻿using BankApp.entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace BankApp.services
+{
+    class BankServices
+    {
+
+        public static string ImportData(string path)
+        { 
+            try
+            {
+                var reader = new StreamReader(path);
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(';');
+
+                    Bank.Register(new Person(
+                        values[0], // Name
+                        int.Parse(values[1]), // AccNumber
+                        DateTime.Parse(values[2]), // AccDate
+                        double.Parse(values[3], new CultureInfo("en-US")) // Balance
+                                    ));
+                }
+                return "Import successful!";
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+
+
+        public static string SaveData()
+        {
+            return "Save sucessesful!";
+        }
+        public static Dictionary<string, Action> Services = new Dictionary<string, Action>();
+
+        public static void RunService(int i)
+        {
+            Action executar = Services.ElementAt(i).Value;
+            executar();
+        }
+    }
+
+}
