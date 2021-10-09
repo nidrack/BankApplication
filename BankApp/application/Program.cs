47d447666ce6a1fa2f8443ebc2b8d63db44da60d
Itamar Takeo <itamar.abe@gmail.com>
2021-10-09 10:28:54 -0300

    Métodos utilizando LINQ em lista e verificação cadastro repetido

--- BankApp/application/Program.cs
+++ BankApp/application/Program.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using System.Collections.Generic;
 
 using BankApp.entities;
+using BankApp.services;
 using System.Linq;
 
 namespace BankApp
@@ -14,13 +15,21 @@ namespace BankApp
         static void Main(string[] args)
         {
 
+            // Bloco de adicionar novo cliente com entrada de dados
+
             // Nome do cliente
             Console.Write("Name: ");
             string name = Console.ReadLine();
 
-            // Número da conta do cliente
+            // Número da conta do cliente sem permitir repetição
             Console.Write("Account Number: ");
             int accNumber = int.Parse(Console.ReadLine());
+            if (Bank.ExistingAccount(accNumber))
+            {
+                Console.WriteLine("This account number is already in use. Try again: ");
+                Console.Write("Account Number: ");
+                accNumber = int.Parse(Console.ReadLine());
+            }
 
             // Data de criação no momento de execução
             DateTime accDate = DateTime.Now;
@@ -31,6 +40,12 @@ namespace BankApp
 
             // Instanciação de um novo cliente na lista de clientes do banco
             Bank.Register(new Person(name, accNumber, accDate, balance));
+
+            // Ver cliente mais antigo registrado
+            Console.WriteLine($"First client: {Bank.FirstClient()}");
+
+            // Ver clientes com saldo menor que 100
+            Bank.PoorClients().ForEach(p => Console.WriteLine(p.ToString()));
 
         }
     }
--- BankApp/entities/Bank.cs
+++ BankApp/entities/Bank.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace BankApp.entities
 {
@@ -11,6 +12,33 @@ namespace BankApp.entities
         public static void Register(Person person)
         {
             clientsList.Add(person);
+        }
+
+        public static bool ExistingAccount(int accNumber)
+        {
+            var listTemp = clientsList
+                .Select(x => x.AccNumber)
+                .ToList();
+            return listTemp.Contains(accNumber);
+        }
+
+        public static string FirstClient()
+        {
+            var listTemp = clientsList
+                .OrderBy(x => x.AccDate)
+                .Take(1)
+                .ToList();
+
+            return $"{listTemp[0].Name} {listTemp[0].AccDate:d}\n";
+        }
+
+        public static List<Person> PoorClients()
+        {
+            var listTemp = clientsList
+                .Where(x => x.Balance < 100.00)
+                .ToList();
+
+            return listTemp;
         }
     }
 }
